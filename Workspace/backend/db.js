e9b2f935710acc0a1ce386d359cc76c2b81facd5
JeroProgramador <jerarroyave6@ielasnieves.edu.co>
2025-10-22 16:27:58 -0500

    Add files via upload

--- Workspace/backend/db.js
+++ Workspace/backend/db.js
@@ -1,14 +1,14 @@
 const mysql = require('mysql2');
-
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'notas'
 });
-
 connection.connect(err => {
     if (err) throw err;
-    console.log('Conectado a la base de datos MYSQL');
+    console.log('Conectado ala base de datos MYSQL');
 });
+
+
 module.exports = connection;
